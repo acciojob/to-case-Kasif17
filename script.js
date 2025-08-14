8f3061f379adafadad647d24285d9ae22e470f27
Mohd Kasif Khan <developer786kasif@gmail.com>
2025-08-14 17:22:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function toCase(text) {
   // write your code here
+	let alower = text.toLowerCase();
+	let blower = text.toUpperCase();
+
+	return alower+"-"+blower
 }
-
 // DO not change the code below
-
 const text = prompt("Enter text:");
 alert(toCase(text));
